a8dcd2b5110f286fbbf59501e7017bdfde592370
Daniel Gomez <dgomezdi@outlook.com>
2018-01-22 08:46:48 -0500

    Add initial view.

--- crypto-watcher/src/app/app.component.ts
+++ crypto-watcher/src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase  } from 'angularfire2/database';
+
+
+
+interface Post {
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,8 +15,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 }) 
 export class AppComponent {
-  messages = [
-    'hello',
-    'hi'
-  ]
+  
+  messages: Observable<any>;
+  constructor(private db: AngularFireDatabase) {
+    
+  }
+
+  ngOnInit (){
+    this.messages = this.db.list("/crypto").valueChanges();
+    // alert(this.messages);
+  }
+  
+  
+  // messages = [ {
+  //   "data": {
+  //     "latestValue": 5239.9466498694255
+  //   },
+  //   "name": "Bitcoin"
+  // },
+  //  {
+  //   "data": {
+  //     "latestValue": 13641.445484626955
+  //   },
+  //   "name": "Ethereum"
+  // }]
 }
--- crypto-watcher/src/app/app.module.ts
+++ crypto-watcher/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppMaterialModuleModule } from './app-material-module/app-material-module.module';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import {AngularFireDatabaseModule, AngularFireDatabase} from 'angularfire2/database';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDCwbUKZdqBF1rb1cRD5nWwqIMX3szL-2w",
+  authDomain: "crypto-monitor-a09a0.firebaseapp.com",
+  databaseURL: "https://crypto-monitor-a09a0.firebaseio.com",
+  projectId: "crypto-monitor-a09a0",
+  storageBucket: "crypto-monitor-a09a0.appspot.com",
+  messagingSenderId: "461645356201"
+};
 
 
 @NgModule({
@@ -14,7 +24,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppMaterialModuleModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig),  // Add this
+    AngularFireDatabaseModule                            // And this
   ],
   providers: [],
   bootstrap: [AppComponent]
